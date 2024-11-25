aa2a695f4b9cc323c797080a80366b82e3b70a46
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-11-25 21:34:26 +0530

    Update 13.Making_a_Meal.cpp

--- 13.Making_a_Meal.cpp
+++ 13.Making_a_Meal.cpp
@@ -10,3 +10,48 @@ Otherwise, we will check whether the occurrences of 'c' and 'e' are more than tw
 so if it true, we will print the minimum value
 otherwise, in else block we will check the minimum of 'c' or 'e', and we print half of that value.
 */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string h="";
+        for(int i=0;i<n;i++){
+            string s;
+            cin >> s;
+            h+=s;
+        }
+        map<char,int>m;
+        m['c']=0;
+        m['o']=0;
+        m['d']=0;
+        m['e']=0;
+        m['h']=0;
+        m['f']=0;
+        for(auto i:h){
+            if(i=='c'||i=='o'||i=='d'||i=='e'||i=='c'||i=='h'||i=='e'||i=='f'){
+                m[i]++;
+            }
+        }
+        int mini=INT_MAX;
+       
+        
+        for(auto i:m){
+            if(mini>i.second){
+                mini=i.second;
+            }
+        }
+        // cout << mini << endl;
+        if(mini == 0)cout << 0 << endl;
+        else if((m['c']>=(2*mini)) && ((m['e'])>=2*mini)) cout<< mini<<endl;
+        else 
+        {
+            int mi=min(m['c'],m['e']);
+            cout<<mi/2<<endl;
+        }
+    }
+}
