57229f7b10662cf36cdbac1e487e7aceb3646fc0
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-12-05 15:01:54 +0530

    Update 25.Power_of_three.cpp

--- 25.Power_of_three.cpp
+++ 25.Power_of_three.cpp
@@ -1,4 +1,4 @@
-//Power of Three
+//Power of Three - leetcode(acc : 47%) (easy)
 //Given an integer n, return true if it is a power of three. Otherwise, return false.
 //An integer n is a power of three, if there exists an integer x such that n == 3x.
 //https://leetcode.com/problems/power-of-three/description/
