2349020fe6567d636e3c159915646a059760061d
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-12-17 19:31:53 +0530

    Create 39.Counting_words.cpp

---
+++ 39.Counting_words.cpp
@@ -0,0 +1,18 @@
+//Counting words - codechef()
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD
+/*
+The question is very clear. 
+We need to do multiply the n and m and print ourput.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>> t;
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+        cout << n * m << endl;
+    }
+}
