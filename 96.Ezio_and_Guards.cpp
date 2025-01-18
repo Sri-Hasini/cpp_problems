02c9956af80d68870d08f76ac837e6937d7bfc56
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-01-18 22:50:12 +0530

    Update and rename 96. to 96.Ezio_and_Guards.cpp

---
+++ 96.Ezio_and_Guards.cpp
@@ -0,0 +1,15 @@
+//Ezio and Guards - codechef(427)
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANIPULATE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+        if (a >= b) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
