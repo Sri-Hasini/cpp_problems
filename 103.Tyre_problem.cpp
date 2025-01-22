88b8bbec3ca58e857165e1c997c7b1912d2882ff
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-01-22 20:40:36 +0530

    Create 103.Tyre_problem.cpp

---
+++ 103.Tyre_problem.cpp
@@ -0,0 +1,14 @@
+//Tyre problem - codechef (452)
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TYRE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        cout << a * 2 + b * 4 << endl;
+    }
+}
