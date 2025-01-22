17e9f10ea009e4f5d7300fd8754b93092423d002
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-01-22 20:18:53 +0530

    Create 100.Maximum_Submissions.cpp

---
+++ 100.Maximum_Submissions.cpp
@@ -0,0 +1,14 @@
+//Maximum Submissions - codechef (435)
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MAXIMUMSUBS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << 2*n << endl;
+    }
+}
