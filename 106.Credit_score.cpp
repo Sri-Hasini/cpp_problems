a8272865a35a666a52685620341ea9671d76fbfe
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-01-23 14:45:49 +0530

    Create 106.Credit_score.cpp

---
+++ 106.Credit_score.cpp
@@ -0,0 +1,11 @@
+//Credit score - codechef(459)
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CREDSCORE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (750 > n) cout << "NO\n";
+    else cout << "YES\n";
+}
