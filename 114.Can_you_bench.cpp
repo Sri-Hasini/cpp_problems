7bb9a8d14cc068dfc4562d841d2db1471d585568
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-01-24 22:01:25 +0530

    Create 114.Can_you_bench.cpp

---
+++ 114.Can_you_bench.cpp
@@ -0,0 +1,15 @@
+//Can you bench - codechef(170)
+//https://www.codechef.com/problems/HJJ
+/*
+They said that 10 is added after every set and asked the total weight of 3rd set.
+It is given 'X' initially and after set 1, 10 is added and after set 2 another 10 is added to X.
+So, we can add 20 directly to the X and display it
+  */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n + 20;
+}
