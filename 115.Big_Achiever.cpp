3b514f07b2f0cdd1d2c89c498ab543b747f29ad6
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-01-24 22:21:37 +0530

    Update 115.Big_Achiever.cpp

--- 115.Big_Achiever.cpp
+++ 115.Big_Achiever.cpp
@@ -1,5 +1,12 @@
 //Big Achiever - codechef (699)
 //https://www.codechef.com/problems/BIG
+/*
+they asked us to print 1 when the current element is greater than before all elements... if no, print 0.
+so we kept some variable maxi to know which element is maximum upto the current - 1 elements... 
+we have created a vector to store answers and initially pushed 1, because for the element at 0th index, there are no before ones to check with....
+we calculated if our maxi is less than current element, we can store 1 in out created vector at current possition otherwise 0, 
+and also update maxi by keeping maximum value of the previous maximum and current element
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
