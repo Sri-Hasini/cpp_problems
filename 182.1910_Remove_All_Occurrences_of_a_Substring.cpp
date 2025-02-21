b7c2b6e686eff2f21717a23a3d288dac1f986f21
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-02-21 10:36:15 +0530

    Update 182.1910_Remove_All_Occurrences_of_a_Substring.cpp

--- 182.1910_Remove_All_Occurrences_of_a_Substring.cpp
+++ 182.1910_Remove_All_Occurrences_of_a_Substring.cpp
@@ -12,11 +12,11 @@ class Solution {
 public:
     string removeOccurrences(string s, string part) {
         while (1) {
-            size_t pos = s.find(part); 
-            if (pos != string::npos) {
-                s.erase(pos, part.length()); 
+            auto pos = s.find(part); //finding whether it have any occurance of the part
+            if (pos != string::npos) {// if it is not equal to the -1 or end i.e., npos here... 
+                s.erase(pos, part.length()); //we can erase starting from the starting iterator of the given substring to length of the substring
             }
-            else break;
+            else break;//if we dont have any occurance further we can break
         }
         return s;
     }
